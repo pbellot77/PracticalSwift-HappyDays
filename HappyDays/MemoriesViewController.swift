0dd51f705e9d2b7ded15391c9544b6e31009b43e
Patrick <patrickbellot77@gmail.com>
2016-10-21 11:57:01 -0500

    remaining methods for first project

--- HappyDays/MemoriesViewController.swift
+++ HappyDays/MemoriesViewController.swift
@@ -20,6 +20,8 @@ class MemoriesViewController: UICollectionViewController, UIImagePickerControlle
   var audioRecorder: AVAudioRecorder?
   var recordingURL: URL!
   var audioPlayer: AVAudioPlayer?
+  var filteredMemories = [URL]()
+  var searchQuery: CSSearchQuery?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -116,6 +118,7 @@ class MemoriesViewController: UICollectionViewController, UIImagePickerControlle
       }
     }
     
+    filteredMemories = memories
     collectionView?.reloadSections(IndexSet(integer: 1))
   }
   
@@ -186,13 +189,13 @@ class MemoriesViewController: UICollectionViewController, UIImagePickerControlle
     if section == 0 {
       return 0
     } else {
-      return memories.count
+      return filteredMemories.count
     }
   }
   
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Memory", for: indexPath) as! MemoryCell
-    let memory = memories[indexPath.row]
+    let memory = filteredMemories[indexPath.row]
     let imageName = thumbnailURL(for: memory).path
     let image = UIImage(contentsOfFile: imageName)
     cell.imageView.image = image
@@ -211,7 +214,7 @@ class MemoriesViewController: UICollectionViewController, UIImagePickerControlle
   }
   
   override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    let memory = memories[indexPath.row]
+    let memory = filteredMemories[indexPath.row]
     let fm = FileManager.default
     
     do {
@@ -237,7 +240,7 @@ class MemoriesViewController: UICollectionViewController, UIImagePickerControlle
       let cell = sender.view as! MemoryCell
       
         if let index = collectionView?.indexPath(for: cell) {
-          activeMemory = memories[index.row]
+          activeMemory = filteredMemories[index.row]
           recordMemory()
         }
       } else if sender.state == .ended {
@@ -368,7 +371,54 @@ class MemoriesViewController: UICollectionViewController, UIImagePickerControlle
       }
   }
   
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    filterMemories(text: searchText)
+  }
   
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    searchBar.resignFirstResponder()
+  }
+  
+  func filterMemories(text: String){
+    guard text.characters.count > 0 else {
+      filteredMemories = memories
+      
+      UIView.performWithoutAnimation {
+        collectionView?.reloadSections(IndexSet(integer: 1))
+      }
+      
+      return
+    }
+    
+    var allItems = [CSSearchableItem]()
+    
+    searchQuery?.cancel()
+    
+    let queryString = "contentDescription == \"*\(text)*\"c"
+    
+    searchQuery = CSSearchQuery(queryString: queryString, attributes: nil)
+    searchQuery?.foundItemsHandler = { items in
+      allItems.append(contentsOf: items)
+    }
+    
+    searchQuery?.completionHandler = { error in
+      DispatchQueue.main.async { [unowned self] in
+        self.activateFilter(matches: allItems)
+      }
+    }
+    
+    searchQuery?.start()
+  }
+  
+  func activateFilter(matches: [CSSearchableItem]){
+    filteredMemories = matches.map { item in
+      return URL(fileURLWithPath: item.uniqueIdentifier)
+    }
+    
+    UIView.performWithoutAnimation {
+      collectionView?.reloadSections(IndexSet(integer: 1))
+    }
+  }
   
   
   
